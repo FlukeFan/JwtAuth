6ca95a135ba99809de2f0f18c37706e94aca6b48
Richard Brown <fluke_fan@hotmail.com>
2021-05-22 16:29:42 +0100

    Added scaffolded identity login/register screen.

---
+++ Modules/Security/Module/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using AuthEx.Security.Data;
+using AuthEx.Shared.Security;
+using FileContextCore;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AuthEx.Security.Areas.Identity.IdentityHostingStartup))]
+namespace AuthEx.Security.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+
+                services.AddDataProtection()
+                    .PersistKeysToFileSystem(new DirectoryInfo("c:\\temp\\auth_ex_dp_keys"))
+                    .SetApplicationName(SecurityConstants.ApplicationName);
+                
+                services.AddDbContext<AuthExSecurityContext>(options =>
+                    options.UseFileContextDatabase(location: "c:\\temp\\auth_ex_db"));
+
+                services.AddDefaultIdentity<AuthExUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthExSecurityContext>();
+
+                services.AddAuthentication(SecurityConstants.AuthenticationScheme);
+
+                services.ConfigureApplicationCookie(opt =>
+                {
+                    opt.Cookie.Name = SecurityConstants.CookieName;
+                    opt.Cookie.Path = "/";
+                });
+            });
+        }
+    }
+}
