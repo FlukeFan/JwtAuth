91afac424b0fed7c29542fcced7efedfb4d58821
Richard Brown <fluke_fan@hotmail.com>
2021-06-02 16:24:48 +0100

    Redirects to login page.

--- Modules/Security/Lib/AuthenticationScheme.cs
+++ Modules/Security/Lib/AuthenticationScheme.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Encodings.Web;
+﻿using System.Security.Claims;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Logging;
@@ -6,7 +7,7 @@ using Microsoft.Extensions.Options;
 
 namespace AuthEx.Security.Lib
 {
-    public class AuthenticationScheme : AuthenticationHandler<AuthenticationScheme.SchemeOptions>
+    public class AuthenticationScheme : SignInAuthenticationHandler<AuthenticationScheme.SchemeOptions>
     {
         public const string Name = "AuthExScheme";
 
@@ -22,6 +23,27 @@ namespace AuthEx.Security.Lib
         {
             await Task.CompletedTask;
             return AuthenticateResult.NoResult();
+        }
+
+        protected override Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            Context.Response.Redirect("/Security/Identity/Account/Login");
+            return Task.CompletedTask;
+        }
+
+        public override Task SignInAsync(ClaimsPrincipal user, AuthenticationProperties properties)
+        {
+            return base.SignInAsync(user, properties);
+        }
+
+        protected override Task HandleSignInAsync(ClaimsPrincipal user, AuthenticationProperties properties)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override Task HandleSignOutAsync(AuthenticationProperties properties)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- Modules/Security/Module/Areas/Identity/IdentityHostingStartup.cs
+++ Modules/Security/Module/Areas/Identity/IdentityHostingStartup.cs
@@ -1,8 +1,7 @@
-﻿using System.IO;
-using AuthEx.Security.Areas.Identity.Data;
+﻿using AuthEx.Security.Areas.Identity.Data;
+using AuthEx.Security.Lib;
 using AuthEx.Shared.Security;
 using FileContextCore;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,24 +14,18 @@ namespace AuthEx.Security.Areas.Identity
         {
             builder.ConfigureServices((context, services) => {
 
-                services.AddDataProtection()
-                    .PersistKeysToFileSystem(new DirectoryInfo("c:\\temp\\auth_ex_dp_keys"))
-                    .SetApplicationName(SecurityConstants.ApplicationName);
-                
                 services.AddDbContext<AuthExSecurityContext>(options =>
                     options.UseFileContextDatabase(location: "c:\\temp\\auth_ex_db"));
 
                 services.AddDefaultIdentity<AuthExUser>(options => options.SignIn.RequireConfirmedAccount = true)
                     .AddEntityFrameworkStores<AuthExSecurityContext>();
 
-                services.AddAuthentication(SecurityConstants.AuthenticationScheme);
-
-                services.ConfigureApplicationCookie(opt =>
-                {
-                    opt.Cookie.Name = SecurityConstants.CookieName;
-                });
-
                 services.AddHostedService<AuthExSecurityContextSetup>();
+
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultSignInScheme = AuthenticationScheme.Name;
+                });
             });
         }
     }
