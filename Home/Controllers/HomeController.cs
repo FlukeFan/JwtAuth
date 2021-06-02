52eab5cea63f583706d9b0db8c98ecde82be0761
Richard Brown <fluke_fan@hotmail.com>
2021-06-02 09:59:03 +0100

    Added example of modified (invalid) JWT (i.e., it is not signed correctly after being modified).

--- Home/Controllers/HomeController.cs
+++ Home/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
-using System.IO;
 using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -31,7 +31,7 @@ namespace AuthEx.Home.Controllers
         {
             var output = "JWT\n\n";
 
-            var provider = new RSACryptoServiceProvider(2048);
+            var provider = new RSACryptoServiceProvider(4096);
             var key = new RsaSecurityKey(provider);
 
             var publicKey = provider.ExportSubjectPublicKeyInfo();
@@ -50,11 +50,20 @@ namespace AuthEx.Home.Controllers
                 Expires = DateTime.UtcNow.AddDays(7),
                 Issuer = myIssuer,
                 Audience = myAudience,
-                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256)
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256Signature)
             };
 
             var token = tokenHandler.CreateToken(tokenDescriptor);
-            output += $"JWT=\n{tokenHandler.WriteToken(token)}\n\n";
+
+            var validJwt = tokenHandler.WriteToken(token);
+
+            var validJwtParts = validJwt.Split('.');
+            var validJwtDecoded = Encoding.ASCII.GetString(Convert.FromBase64String(validJwtParts[1]));
+            var invalidJwtPart = validJwtDecoded.Replace("user1", "user2");
+            var invalidJwt = $"{validJwtParts[0]}.{Convert.ToBase64String(Encoding.ASCII.GetBytes(invalidJwtPart))}.{validJwtParts[2]}";
+
+            output += $"valid JWT=\n{validJwt}\n\n";
+            output += $"invalid JWT=\n{invalidJwt}\n\n";
             output += $"pub=\n-----BEGIN PUBLIC KEY-----{pub}-----END PUBLIC KEY-----\n\n";
 
             return Content(output);
