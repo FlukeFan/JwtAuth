e3b6f5b79f59f4f39ddf783e326ea546eb5c21ef
Richard Brown <fluke_fan@hotmail.com>
2021-06-02 08:52:55 +0100

    Example of creating a JWT (from here:  https://dotnetcoretutorials.com/2020/01/15/creating-and-validating-jwt-tokens-in-asp-net-core/)

--- Home/Controllers/HomeController.cs
+++ Home/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AuthEx.Home.Controllers
 {
@@ -19,6 +24,35 @@ namespace AuthEx.Home.Controllers
         public IActionResult Page2()
         {
             return View();
+        }
+
+        public IActionResult Jwt()
+        {
+            var output = "JWT\n\n";
+
+            var mySecret = "asdv234234^&%&^%&^hjsdfb2%%%";
+            var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
+
+            var myIssuer = "http://localhost:8124";
+            var myAudience = "http://localhost:8124";
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.NameIdentifier, "user1"),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                Issuer = myIssuer,
+                Audience = myAudience,
+                SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            output += $"JWT={tokenHandler.WriteToken(token)}";
+
+            return Content(output);
         }
     }
 }
