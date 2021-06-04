a66272cf8a064af22dcb4a7f45063b5ee7650aa0
Richard Brown <fluke_fan@hotmail.com>
2021-06-04 16:32:52 +0100

    Minor tidy up.

--- Home/Startup.cs
+++ Home/Startup.cs
@@ -22,7 +22,6 @@ namespace AuthEx.Home
         public IConfiguration Configuration { get; }
         public IWebHostEnvironment HostEnvironment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddZipDeploy();
