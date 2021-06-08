0bdeff5d79af94238ed67997998533c07e59a6b8
Richard Brown <fluke_fan@hotmail.com>
2021-06-08 10:50:51 +0100

    Added tools for secure FTP upload.

--- CommandPrompt.bat
+++ CommandPrompt.bat
@@ -2,7 +2,9 @@
 @title AuthEx Command Prompt
 @SET PATH=C:\Program Files\dotnet\;%PATH%
 @SET PATH=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\;%PATH%
-@REM type readme.md
+@type readme.md
+@doskey p=dotnet msbuild build.proj /t:Package $*
+@doskey d=dotnet msbuild build.proj /t:Deploy $*
 @echo.
 @echo Aliases:
 @echo.
